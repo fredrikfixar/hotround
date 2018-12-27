fd72490a9a058f6d4be5d165b539eeec85bbceeb
fredrikfixar <fredrikfixar@gmail.com>
2018-12-27 14:43:47 +0100

    using tutorial for konva as vue component

--- src/components/KonvaTest.vue
+++ src/components/KonvaTest.vue
@@ -1,147 +1,90 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-stage ref="stage"
+      :config="configKonva"
+      @dragstart="handleDragstart"
+      @dragend="handleDragend">
+      <v-layer ref="layer">
+        <v-star
+          v-for="item in list"
+          :key="item.id"
+          :config="item"></v-star>
+      </v-layer>
+      <v-layer ref="dragLayer"></v-layer>
+    </v-stage>
+  </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
-
-      ]
-    })
+const width = window.innerWidth;
+const height = window.innerHeight;
+let vm = {};
+export default {
+  data() {
+    return {
+      list: [],
+      configKonva: {
+        width: width,
+        height: height
+      }
+    };
+  },
+  methods: {
+    handleDragstart(e) {
+      const shape = e.target;
+      const dragLayer = vm.$refs.dragLayer.getNode();
+      const stage = vm.$refs.stage.getNode();
+      // moving to another layer will improve dragging performance
+      shape.moveTo(dragLayer);
+      stage.draw();
+      shape.setAttrs({
+        shadowOffsetX: 15,
+        shadowOffsetY: 15,
+        scaleX: shape.getAttr('startScale') * 1.2,
+        scaley: shape.getAttr('startScale') * 1.2
+      });
+    },
+    handleDragend(e) {
+      const shape = e.target;
+      const layer = vm.$refs.layer.getNode();
+      const stage = vm.$refs.stage.getNode();
+      shape.moveTo(layer);
+      stage.draw();
+      shape.to({
+        duration: 0.5,
+        easing: Konva.Easings.ElasticEaseOut,
+        scaleX: shape.getAttr('startScale'),
+        scaleY: shape.getAttr('startScale'),
+        shadowOffsetX: 5,
+        shadowOffsetY: 5
+      });
+    }
+  },
+  mounted() {
+    vm = this;
+    for (let n = 0; n < 30; n++) {
+      const scale = Math.random();
+      const stage = vm.$refs.stage.getStage();
+      vm.list.push({
+        x: Math.random() * stage.getWidth(),
+        y: Math.random() * stage.getHeight(),
+        rotation: Math.random() * 180,
+        numPoints: 5,
+        innerRadius: 30,
+        outerRadius: 50,
+        fill: '#89b717',
+        opacity: 0.8,
+        draggable: true,
+        scaleX: scale,
+        scaleY: scale,
+        shadowColor: 'black',
+        shadowBlur: 10,
+        shadowOffsetX: 5,
+        shadowOffsetY: 5,
+        shadowOpacity: 0.6,
+        startScale: scale
+      });
+    }
   }
+};
 </script>
-
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import VueKonva from 'vue-konva'
 
 Vue.config.productionTip = false
+
+Vue.use(VueKonva)
 
 new Vue({
   render: h => h(App),
